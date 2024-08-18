aecbfd75461b08289a32d7196d726a74c437efc8
anantakoirala <ananta.drn2049@gmail.com>
2024-08-18 14:27:58 +0545

    added allowed headers

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify the allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed headers
   })
 );
 
