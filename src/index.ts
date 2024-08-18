af20dd7f17bcf598ffbf9772a8e0a7c799152daa
anantakoirala <ananta.drn2049@gmail.com>
2024-08-18 14:38:55 +0545

    manually adding cors header

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,15 @@ const io = new Server(httpServer, {
   },
 });
 
+// Manually set the Access-Control-Allow-Origin header before your routes
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL); // Your frontend URL
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Create a PeerJS server
 const peerServer = ExpressPeerServer(httpServer, {
   debug: true,
